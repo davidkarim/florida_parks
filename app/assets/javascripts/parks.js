4d83a10b9f11189697e3e075f4a9592514e5bdd3
David Karim <davidkarim@gmail.com>
2016-06-26 18:32:17 -0400

    Added filtering by city name

--- app/assets/javascripts/parks.js
+++ app/assets/javascripts/parks.js
@@ -1,14 +1,23 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
+function ajax_request() {
+  // Send name and city via ajax request. Replace any spaces with underscores
+  input_name_val = document.getElementById('park_name').value.split(" ").join("_");
+  input_city_val = document.getElementById('park_city').value.split(" ").join("_");
+  // load_val = '/parks/refresh?input_name=' + input_val
+  load_val = '/parks/refresh?input_name=' + input_name_val + '&input_city=' + input_city_val;
+  $('.dynamic').load(load_val);
+}
+
 $(document).ready(
         function() {
+          // Event handler for keyup events while typing name
           $("#park_name").keyup(function(event) {
-            console.log(event);
-            console.log(event.key);
-            console.log()
-            input_val = document.getElementById('park_name').value
-            load_val = '/parks/refresh?input=' + input_val
-            $('.dynamic').load(load_val);
+            ajax_request();
+          });
+          // Event handler for keyup events while typing city
+          $("#park_city").keyup(function(event) {
+            ajax_request();
           });
         });
--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,7 +1,8 @@
 class ParksController < ApplicationController
   def refresh_partial
-    ini_string = params[:input]
-    @parks = Park.where "name LIKE '#{ini_string}%'"
+    ini_name = params[:input_name]
+    ini_city = params[:input_city]
+    @parks = Park.where "name LIKE '%#{ini_name}%' AND city LIKE '%#{ini_city}%'"
     # binding.pry
     @park = Park.new
     render partial: "table"
