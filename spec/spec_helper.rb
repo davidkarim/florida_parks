d2fddb9b12d039b925feda2693b14bf097921f61
David Karim <david.karim@arthrex.com>
2018-01-30 19:59:18 -0500

    Save simplecov to CircleCI's artifacts directory if we're on CircleCI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -26,6 +26,13 @@ SimpleCov.formatters = [
   SimpleCov::Formatter::JSONFormatter,
   SimpleCov::Formatter::RcovFormatter
 ]
+
+# save to CircleCI's artifacts directory if we're on CircleCI
+if ENV['CIRCLE_ARTIFACTS']
+  dir = File.join(ENV['CIRCLE_ARTIFACTS'], "coverage")
+  SimpleCov.coverage_dir(dir)
+end
+
 SimpleCov.start
 
 RSpec.configure do |config|
